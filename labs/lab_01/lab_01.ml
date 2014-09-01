284e578d9bd287462ecd4df20811b281a8283f85
Daniel DaCosta <chaosape@chaosape.com>
2014-09-01 13:39:54 -0500

    'Modification of student directory for lab 1.'

---
+++ labs/lab_01/lab_01.ml
@@ -0,0 +1,11 @@
+(* Modified by: ... replace the text between the dots with your name ... *)
+
+(* A function computer the Fibonacci sequence: 1, 1, 2, 3, 5, 8, ... *)
+
+(* There is a bug in the following program.  Can you fix it? *)
+
+let rec fib x =
+  if x < 3 then 1 else fib (n-1) + fib (n-1)
+
+
+
