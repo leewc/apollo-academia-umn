b8141795612b400a8344388ceec6e494469163f4
Wen Chuan Lee <leex7095@umn.edu>
2014-09-02 15:25:16 -0500

    Fixed the bug in Fib

--- labs/lab_01/lab_01.ml
+++ labs/lab_01/lab_01.ml
@@ -1,11 +1,11 @@
 (* Modified by: ... Wen Chuan Lee ... *)
 
-(* A function computer the Fibonacci sequence: 1, 1, 2, 3, 5, 8, ... *)
+(* A function compute the Fibonacci sequence: 1, 1, 2, 3, 5, 8, ... *)
 
 (* There is a bug in the following program.  Can you fix it? *)
 
 let rec fib x =
-  if x < 3 then 1 else fib (n-1) + fib (n-1)
+  if x < 3 then 1 else fib (x-1) + fib (x-2)
 
 
 
