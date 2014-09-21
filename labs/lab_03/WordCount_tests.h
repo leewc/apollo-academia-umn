2a7c7303cbde104b2cc36487e2f0511200721eeb
Wen Chuan Lee <leewenchuan.lwc@gmail.com>
2014-09-21 02:29:09 -0500

    Added Makefile and answers.txt

--- labs/lab_03/WordCount_tests.h
+++ labs/lab_03/WordCount_tests.h
@@ -51,6 +51,14 @@ public:
     /* These tests are like those above, they help us understand how
        to use the functions.  */
 
+    // Add: T1 test that intentionally fails
+    void test_makeRegex_fail_illformed_backets() {
+      regex_t *re = makeRegex ("(while)");
+      TS_ASSERT (re) ; 
+      int numMatchedChars = matchRegex (re, "while wah");
+      TS_ASSERT_EQUALS (numMatchedChars, 5);
+    }
+
     void test_matchRegex_abc_anywhere () {
         regex_t *re = makeRegex ("abc") ;
         TS_ASSERT (re) ;
@@ -96,6 +104,16 @@ public:
        These create the regex_t and then try to match it.  These only
        need match at the beginning of the text. */
 
+
+    //Add: Pattern not present in the text, hence fails to match with 0 chars
+    void test_match_makeRegex_pattern_not_present () {
+      regex_t *re = makeRegex("[0-9]*");
+      TS_ASSERT (re);
+      int numMatchedChars = matchRegex(re, "a1234");
+      TS_ASSERT_EQUALS (numMatchedChars, 0);
+    }
+
+
     void test_make_matchRegex_word () {
         regex_t *re = makeRegex ("^([a-zA-Z]+)") ;
         TS_ASSERT (re) ;
@@ -110,6 +128,45 @@ public:
 
        Write your tests below.
     */
+
+
+    //Add: Tests that match other wordcounts
+    void test_make_matchRegex_integerConsts () {
+        regex_t *re = makeRegex ("^[0-9]*") ;
+        TS_ASSERT (re) ;
+        int numMatchedChars = matchRegex (re, "12345") ;
+        TS_ASSERT_EQUALS (numMatchedChars, 5) ;
+    }
+
+
+    void test_make_matchRegex_whiteSpace (){
+        regex_t *re = makeRegex ("^[\n\t\r ]+") ;
+        TS_ASSERT (re) ;
+        int numMatchedChars = matchRegex (re, "            \n") ;
+        TS_ASSERT_EQUALS (numMatchedChars, 13) ;
+    }
+
+    void test_make_matchRegex_blockComments (){
+        regex_t *re = makeRegex ("^/\\*([^\\*]|\\*+[^\\*/])*\\*+/") ;
+        TS_ASSERT (re) ;
+        int numMatchedChars = matchRegex (re, "/* */") ;
+        TS_ASSERT_EQUALS (numMatchedChars, 5) ;
+    }
+
+    void test_make_matchRegex_floatingPointConsts () {
+        regex_t *re = makeRegex ("^[0-9]*.[0-9]+") ;
+        TS_ASSERT (re) ;
+        int numMatchedChars = matchRegex (re, ".2345") ;
+        TS_ASSERT_EQUALS (numMatchedChars, 5) ;
+    } 
+
+    void test_make_matchRegex_Boot () {
+        regex_t *re = makeRegex ("^(Boot)") ;
+        TS_ASSERT (re) ;
+        int numMatchedChars = matchRegex (re, "boot123") ;
+        TS_ASSERT_EQUALS (numMatchedChars, 0) ;
+    }
+
 
 
     // Tests for consumeWhiteSpaceAndComments.
@@ -128,7 +185,35 @@ public:
 
        Write your tests below.
     */
+    	void test_consumeWhiteSpaceAndComments_1 () {
+        regex_t *whiteSpace = makeRegex ("^[\n\t\r ]+") ;
+	regex_t *blockComments = makeRegex ("^/\\*([^\\*]|\\*+[^\\*/])*\\*+/") ;
+	regex_t *lineComment = makeRegex ("^//[^\n]*\n");
+        
+        int numMatchedChars = consumeWhiteSpaceAndComments(whiteSpace,blockComments,lineComment, 
+			"  	/* 		*/  // ") ;
+        TS_ASSERT_EQUALS (numMatchedChars, 12) ;
+    }
 
+	void test_consumeWhiteSpaceAndComments_empty () {
+        regex_t *whiteSpace = makeRegex ("^[\n\t\r ]+") ;
+	regex_t *blockComments = makeRegex ("^/\\*([^\\*]|\\*+[^\\*/])*\\*+/") ;
+	regex_t *lineComment = makeRegex ("^//[^\n]*\n");
+        
+        int numMatchedChars = consumeWhiteSpaceAndComments(whiteSpace,blockComments,lineComment,"") ;
+        TS_ASSERT_EQUALS (numMatchedChars, 0) ;
+    }
+
+	void test_consumeWhiteSpaceAndComments_mix () {
+        regex_t *whiteSpace = makeRegex ("^[\n\t\r ]+") ;
+	regex_t *blockComments = makeRegex ("^/\\*([^\\*]|\\*+[^\\*/])*\\*+/") ;
+	regex_t *lineComment = makeRegex ("^//[^\n]*\n");
+        
+        int numMatchedChars = consumeWhiteSpaceAndComments(whiteSpace,blockComments,lineComment, 
+				"//a comment \n notAComment") ;
+        TS_ASSERT_EQUALS (numMatchedChars, 14) ;
+    }
+    
 
     // Tests for wordCount.
     // --------------------------------------------------
@@ -149,7 +234,22 @@ public:
         TS_ASSERT_EQUALS (wordCount (" someting ").numWords, 1 )  ;        
     }
 
+    void test_wordCount_single_word_Boot () {
+        TS_ASSERT_EQUALS (wordCount (" Boot ").numBoots, 1 )  ;        
+    }
 
+    void test_wordCount_single_word_int () {
+        TS_ASSERT_EQUALS (wordCount (" 123 ").numNumericConsts, 1 )  ;        
+    }
+
+
+    void test_wordCount_single_word_float () {
+        TS_ASSERT_EQUALS (wordCount (" 0.452 ").numNumericConsts, 1 )  ;        
+    }
+
+    void test_wordCount_single_notWord () { 
+	TS_ASSERT_EQUALS (wordCount (" /*not a word */").numWords, 0);
+    }
     /* T6. (2 tests) We can now write some tests that include several
        elements. These test should check the contents of the returned
        structure to see that the right number of words, numbers and
@@ -162,7 +262,20 @@ public:
        Write your tests below.
      */
 
+    void test_WordCount_mix_empty () {
+	Results re = wordCount("");
+        TS_ASSERT_EQUALS (re.numWords, 0);
+	TS_ASSERT_EQUALS (re.numNumericConsts, 0);
+	TS_ASSERT_EQUALS (re.numBoots, 0);
+    }
 
+
+    void test_WordCount_mix () {
+	Results re = wordCount("A boot of 3.142 is not a Boot");
+        TS_ASSERT_EQUALS (re.numWords, 6);
+	TS_ASSERT_EQUALS (re.numNumericConsts, 1);
+	TS_ASSERT_EQUALS (re.numBoots, 1);
+    }
 } ;
 
 
