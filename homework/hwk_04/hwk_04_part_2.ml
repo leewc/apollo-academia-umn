e0a885661e270da3789e1dabeaf634b32b5b78f0
Wen Chuan Lee <leewenchuan.lwc@gmail.com>
2014-10-06 01:00:59 -0500

    completed eval and almost done with freevars, last test case fails

--- homework/hwk_04/hwk_04_part_2.ml
+++ homework/hwk_04/hwk_04_part_2.ml
@@ -5,8 +5,8 @@ type expr
   | Div of expr * expr
   | Let of string * expr * expr
   | Var of string
-  | LT of expr * expr
-  | EQ of expr * expr
+  | LT of expr * expr (*less than*)
+  | EQ of expr * expr (*equal *)
   | And of expr * expr
   | Not of expr
   | IfThenElse of expr * expr * expr
@@ -29,13 +29,100 @@ let eval (e:expr) : value =
         in (match v1,v2 with 
             | IntVal x, IntVal y -> IntVal (x + y)
             | _ -> raise (Failure "Addition requires 2 integer values."))
-
+    | Mul (l,r) ->
+        let v1 = eval_h l env  and  v2 = eval_h r env
+        in (match v1,v2 with 
+            | IntVal x, IntVal y -> IntVal (x * y)
+            | _ -> raise (Failure "Multiplication requires 2 integer values."))
+    | Sub (l,r) ->
+        let v1 = eval_h l env  and  v2 = eval_h r env
+        in (match v1,v2 with 
+            | IntVal x, IntVal y -> IntVal (x - y)
+            | _ -> raise (Failure "Subtraction requires 2 integer values."))
+    | Div (l,r) ->
+        let v1 = eval_h l env  and  v2 = eval_h r env
+        in (match v1,v2 with 
+            | IntVal x, IntVal y -> IntVal (x / y)
+            | _ -> raise (Failure "Division requires 2 integer values."))
+    | LT (l,r) ->
+       let v1 = eval_h l env  and  v2 = eval_h r env
+       in (match v1,v2 with 
+            | IntVal x, IntVal y -> if x < y then BoolVal true else BoolVal false
+            | _ -> raise (Failure "Less than operator requires 2 integer values."))
+    | EQ (l,r) ->
+       let v1 = eval_h l env  and  v2 = eval_h r env
+       in (match v1,v2 with 
+            | IntVal x, IntVal y -> if x = y then BoolVal true else BoolVal false
+	    | BoolVal x, BoolVal y -> if x=y then BoolVal true else BoolVal false
+            | _ -> raise (Failure "Equality test requires 2 values of the same type."))
+    | And (l,r) ->
+       let v1 = eval_h l env  and  v2 = eval_h r env
+       in (match v1,v2 with 
+            | BoolVal x, BoolVal y -> if x && y then BoolVal true else BoolVal false
+            | _ -> raise (Failure "And operator requires 2 boolean values."))
+    | Not a ->
+       let v1 = eval_h a env 
+       in (match v1 with 
+	   | BoolVal x -> BoolVal (not x)
+	   | _ -> raise (Failure "Not operator requires a boolean value."))
+    | IfThenElse (b2,l,r) ->
+       let b = eval_h b2 env
+       in   
+         (match b with 
+            | BoolVal b -> if b=true then (eval_h l env) else (eval_h r env)
+            | _ -> raise (Failure "If then else requires one Boolean value and 2 integer/boolean values."))
+    | Let (var, def, ex) -> eval_h ex ( (var, eval_h def env)::env)
+    | Var x -> lookup x env
     | IntConst v -> IntVal v
     | BoolConst b -> BoolVal b
 
-    (* Many cases missing here, you need to complete them. *)
 
   in eval_h e []
+
+let freevars (a:expr):string list =
+(*
+  let rec inList (a:string) (b:string list) = 
+    match b with 
+    | x::rest -> if x = a then true else inList a rest
+    | [] -> false
+  in 
+
+  let rec clean (a:string list) (refa:string list) (out:string list):string list = 
+    match a with 
+    | x::rest -> if not (inList x refa) then clean rest refa out@[x] else clean rest refa out
+    | [] -> out
+  in 
+ *)
+  let rec make_str (a:expr) (validate:string list):string list =
+    match a with
+
+(*    | Var x -> if not (inList x validate) then [x] else [] *)
+    | Var x -> [x] 
+    | Let (var,def,ex) -> (match def with
+			   | IntConst _
+(*
+			   | BoolConst _ -> (if (inList var validate) then make_str ex validate@[var] 
+					     else make_str ex validate)
+ *)
+			   | BoolConst _-> []
+			   | _ -> [var])
+    | Mul (x,y)
+    | Div (x,y)
+    | Sub (x,y)
+    | Add (x,y) 
+    | LT (x,y)
+    | EQ (x,y)
+    | And (x,y)
+      -> make_str x validate @ make_str y validate
+    | Not x -> make_str x validate
+    | IfThenElse (x,y,z) -> make_str x validate @ make_str y validate @ make_str z validate
+    | _ -> []
+   in  
+   let rec lookup n env out validate = 
+    match env with 
+    | [] -> out@make_str n validate
+    | (name,value)::rest -> if n = name then value else lookup n rest out (validate)
+   in (lookup a [] [] [])
 
 
 
