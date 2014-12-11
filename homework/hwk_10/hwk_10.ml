39148a8a9d141cf1bfd58ff3a57fbe02ead3b7e1
Wen Chuan Lee <leewenchuan.lwc@gmail.com>
2014-12-10 20:19:08 -0600

    Finished hwk_10

--- homework/hwk_10/hwk_10.ml
+++ homework/hwk_10/hwk_10.ml
@@ -1,17 +1,4 @@
-(*Written Code - With Inspirations frm RealWorldOcaml*)
-open Core.Std
-
-(*some helper functions*)
-let rec map (f:'a -> 'b) (l:'a list) : 'b list =
-  match l with
-  | [] -> []
-  | x::xs -> f x :: map f xs
-
-let rec foldl f v l =
-  match l with
-  | [] -> v
-  | x::xs ->  foldl f (f v x) xs
-
+(*Code - With Inspirations frm RealWorldOcaml*)
 (*Signature aka Interface for the Int/Complex Arith modules*)
 module type Arithmetic_intf = sig 
     type t (*will hold interval, floats,ints, complex*)
@@ -32,7 +19,7 @@ module Complex_arithmetic : (Arithmetic_intf with type t = (float * float)) = st
   type t = (float * float) 
   let to_string (x,y) = "(" ^ Float.to_string x ^ "+"^Float.to_string y ^"i)"
   let add (x1,y1) (x2,y2) = (x1 +. y1, x2 +. y2)   (*where y1 and y2 are the complex numbers*)
-  let mul (x1,y1) (x2,y2) = ((x1 *. x2 -. y1*.y2) , (x1 *. y2 +. y1 *. x2)) 
+  let mul (a,bi) (c,di) = (a *. c +. -1.0 *. (bi *. di) , a *. di +. c *. bi) 
 end 
 
 (*Signature for Int_Vector and Complex_Vector modules*)
@@ -47,7 +34,6 @@ module type Vector_intf = sig
     val scalar_prod : vector -> vector -> t option
     val to_string : vector -> string 
     val size: vector -> int
-
   end
 
 (*Functor that takes in module of type Int/Complext arithmetic
@@ -57,11 +43,22 @@ module Make_vector(Arith_mod : Arithmetic_intf) :
     (Vector_intf with type t = Arith_mod.t) = struct
   type vector = Vector of Arith_mod.t list  (*if I do vector = Arith_mod.t it just masks it, not a good practice*)
   type t = Arith_mod.t
-   
+  
+  (*some helper functions*)
+  let rec map (f:'a -> 'b) (l:'a list) : 'b list =
+    match l with
+    | [] -> []
+    | x::xs -> f x :: map f xs
+
+  let rec foldl f v l =
+    match l with
+    | [] -> v
+    | x::xs ->  foldl f (f v x) xs
+  (*end helper functions*)
+
   let create (size:int) (initVal:t):vector= 
-    let rec create_list (size:int) (initVal:t) = 
-      if size = 0 then [] 
-      else (initVal::(create_list (size-1) initVal))
+    let rec create_list (size:int) (initVal:t) =  if size = 0 then [] 
+						  else (initVal::(create_list (size-1) initVal))
     in Vector (create_list size initVal)
 
   let from_list (tList: t list): vector = Vector tList
@@ -99,16 +96,14 @@ module Make_vector(Arith_mod : Arithmetic_intf) :
     in 
     match vec with 
     | Vector l -> "<< " ^ Int.to_string (size (vec)) ^ " | " ^ hlpr l ^ " >>" 
-end 
+end
  
-module Int_vector = Make_vector(Int_arithmetic)
-module Complex_vector = Make_vector(Complex_arithmetic)
-
-
 (*
   Signature defining the type and ops for the numeric vals in the vector
   Modules implementing the signature wil be the input module to the functor
-*)
+*) 
+module Int_vector = Make_vector(Int_arithmetic)
+module Complex_vector = Make_vector(Complex_arithmetic)
 
 let v1 = Int_vector.create 10 1
 let v2 = Int_vector.from_list [1;2;3;4;5]
@@ -122,11 +117,10 @@ let s2 = Int_vector.to_string v2
 let s3 = Int_vector.to_string v3
 let s4 = Int_vector.to_string v4
 
-
 let v5 = Complex_vector.from_list [ (1.0, 2.0); (3.0, 4.0); (5.0, 6.0) ]
 let v6 = Complex_vector.scalar_add (5.0, 5.0) v5
-let testmul = Complex_vector.scalar_mul (5.0, 5.0) v5
-let stestmul = Complex_vector.to_string testmul
-(*let c1 = Complex_vector.scalar_prod v5 v6 *)
+let cmplxmul = Complex_vector.scalar_mul (5.0, 5.0) v5
+let scmplx = Complex_vector.to_string cmplxmul
+let c1 = Complex_vector.scalar_prod v5 v6 
 let s5 = Complex_vector.to_string v5
 let s6 = Complex_vector.to_string v6
