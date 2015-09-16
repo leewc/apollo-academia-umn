a0fb4a0acbe864b4b05b48821063623e2053c584
Wen Chuan Lee <leewenchuan.lwc@gmail.com>
2015-09-16 01:32:09 -0500

    More code, fix pointer arithmetic, added makefile

--- minsh.c
+++ minsh.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 void shell_loop();
-char* getInputWithoutDelimiter();
+int getInputWithoutDelimiter(char** input);
+int makeargv(const char *s, const char *delimiters, char ***argvp);
+
 
 int main(int argc, char *argv[])
 {
@@ -29,10 +33,10 @@ void shell_loop()
   int run = 1;
   while(run)
   {
-    char *input;
+    char *input = NULL;
 
     printf("-$ ");
-    input = getInputWithoutDelimiter();
+    getInputWithoutDelimiter(&input);
     printf("%s \n", input);
 
     const char *ex = "exit";
@@ -45,18 +49,79 @@ void shell_loop()
   }
 }
 
-char* getInputWithoutDelimiter()
+int getInputWithoutDelimiter(char** input)
 {
-  char* input = NULL;
   size_t len = 0;
-  ssize_t numchars = getline(&input, &len, stdin);
+  ssize_t numchars = getline(input, &len, stdin);
   
   if(numchars == -1)
-    fprintf(stderr, "Failed to read input. \n");
-  if(input[numchars-1] == '\n')
   {
-    input[numchars-1] = '\0'; //null terminator
+    fprintf(stderr, "Failed to read input. \n");
+    return -1;
+  }
+
+  if(*( *input + numchars -1)  == '\n')
+  {
+    *(*input + numchars -1) = '\0'; //null terminator
     --numchars;
   }
-  return input;
+
+  return numchars;
+}
+
+
+int makeargv(const char *s, const char *delimiters, char ***argvp)
+{
+  int error;
+  int i;
+  int numTokens;
+  const char *snew;
+  char *t;
+
+  if ((s == NULL) || (delimiters == NULL) || (argvp == NULL))
+  {
+    errno = EINVAL;
+    return -1;
+  }
+
+  *argvp = NULL; //pointer to arrray of strings initialiezed to NULL
+
+  /* Pointer arithmethic to make snew point to the real start of the 
+   * string, as strspn returns the length of s which consists of 
+   * delimiters. Hence snew is real start of string.
+   */
+  snew = s + strspn(s, delimiters); 
+
+  // allocate memory for t
+  if ((t = malloc(strlen(snew) + 1)) == NULL) // +1 for null term
+    return -1;
+
+  strcpy(t, snew); //strtok does in place tokenization
+  numTokens = 0;
+  if (strtok(t, delimiters) != NULL)
+    // count number of tokens (to allocate buffer)
+    for (numTokens = 1; strtok(NULL, delimiters) != NULL; numTokens++);
+
+  // allocate mem for array of pointers to tokens
+  if (( *argvp = malloc((numTokens + 1) * sizeof(char *))) == NULL)
+  {
+    error = errno;
+    free(t);
+    errno = error;
+    return -1;
+  }
+
+  if (numTokens == 0)
+    free(t);
+  else 
+  {
+    strcpy(t, snew);
+    **argvp = strtok(t, delimiters);
+    for(i = 1; i < numTokens; i++)
+      *( (*argvp) + i) = strtok(NULL, delimiters); 
+  }
+  
+  *( (*argvp) + numTokens) = NULL; //put null pointer at end of argvp array
+
+  return numTokens;
 }
