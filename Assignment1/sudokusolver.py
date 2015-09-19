d8433dc2dc0e2a615f1597eab56937f9d87fa9a3
Wen Chuan Lee <leewenchuan.lwc@gmail.com>
2015-09-19 17:55:20 -0500

    Wrong implementation, probably have to start over

--- Assignment1/sudokusolver.py
+++ Assignment1/sudokusolver.py
@@ -1,3 +1,5 @@
+import queue
+
 """This is where the problem is defined. Initial state, goal state and other information that can be got from the problem"""
 
 
@@ -11,6 +13,8 @@ class Problem(object):
         - since each level is based on numbers, create nodes with value 1 -> length
         - goal test needs to check if the constraints are obeyed.
         - perhaps add a separate flag to enable pruning? (later)
+
+        Problem inherits from Object, so any representation can be used with this class
     """
 
     def __init__(self, initial, goal=None):
@@ -20,26 +24,55 @@ class Problem(object):
             Based on the set up initial is of type node. A Tree of Nodes.
             Goal is also a node tree.
         """
-        self.initial = initial
+        self.initial = initial      # the sudoku representation
         self.goal = goal
+        self.numberOfUnknowns = 0
+        self.dimension = len(initial) # dimension of sudoku
+
+        for row in initial:
+            # 0 represents an unknown number
+                self.numberOfUnknowns += row.count(0) 
 
     def actions(self, state):
         """Return the actions that can be executed in the given
         state. The result would typically be a list, but if there are
         many actions, consider yielding them one at a time in an
         iterator, rather than building them all at once."""
+        if state.depth != self.numberOfUnknowns:
+            return "GenerateChildNode"
+        else:
+            return "GenerateSolution"
 
     def result(self, state, action):
         """Return the state that results from executing the given
         action in the given state. The action must be one of
         self.actions(state)."""
+        # WC - result returns a node with the action it was created with
+        if action is "GenerateChildNode":
+            for i in range (0, dimension):
+                newNode = Node(i, parent=state, action=action)
+                yield newNode
+
+        if action is "GenerateSolution":
+            while state.parent != self.initial:
+                possibleSolution = state
+                state = state.parent
+                yield possibleSolution
+
+
 
     def goal_test(self, state):
         """Return True if the state is a goal. The default method compares the
         state to self.goal, as specified in the constructor. Override this
         method if checking against a single self.goal is not enough.
         This must be written by students"""
-        return False
+        if not isinstance(state, int): 
+            return False
+        if state.depth != self.numberOfUnknowns:
+            return False
+        else:
+            # At the bottom of the tree, time to check if solution is complaint.
+            raise(NotImplementedError)
 
 
 class Node:
@@ -57,7 +90,11 @@ class Node:
     def __init__(self, state, parent=None, action=None):
         """Create a search tree Node, derived from a parent by an action.
         Update the node parameters based on constructor values"""
-        update(self, state=state, parent=parent, action=action, depth=0)
+        # update(self, state=state, parent=parent, action=action, depth=0)
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.depth = 0
         # If depth is specified then depth of node will be 1 more than the
         # depth of parent
         if parent:
@@ -65,12 +102,13 @@ class Node:
 
     def expand(self, problem):
         # List the nodes reachable in one step from this node.
+        # WC: For each action in problem.action return the child_node that is the result of the action
         return [self.child_node(problem, action)
                 for action in problem.actions(self.state)]
 
     def child_node(self, problem, action):
         next = problem.result(self.state, action)
-        return Node(next, self, action)
+        yield Node(next, self, action)
 
 
 def breadth_first_search(problem):
@@ -81,14 +119,14 @@ def breadth_first_search(problem):
         return node
     # Create a Queue to store all nodes of a particular level. Import
     # QueueClass()
-    frontier = QueueClass()
-    frontier.enqueue(node)
+    frontier = queue.Queue()
+    frontier.put(node)
 
     # Loop until all nodes are explored(frontier queue is empty) or Goal_Test
     # criteria are met
     while frontier:
         # Remove from frontier, for analysis
-        node = frontier.dequeue()
+        node = frontier.get()
         # Loop over all children of the current node
         # Note: We consider the fact that a node can have multiple child nodes
         # here
@@ -97,5 +135,22 @@ def breadth_first_search(problem):
             if problem.goal_test(child.state):
                 return child
             # Add every new child to the frontier
-            frontier.enqueue(node)
+            frontier.put(node)
     return None
+
+
+
+def runApp():
+    sudoku = [
+              [1, 5, 0, 0, 4, 0],
+              [2, 0, 0, 0, 5, 6],
+              [4, 0, 0, 0, 0, 3],
+              [0, 0, 0, 0, 0, 4],
+              [6, 3, 0, 0, 2, 0],
+              [0, 2, 0, 0, 3, 1],
+             ]
+    # problemStartNode = Node(sudoku) -- This is done by BFS
+    breadth_first_search(Problem(sudoku))
+
+if(__name__ == '__main__'):
+    runApp()
