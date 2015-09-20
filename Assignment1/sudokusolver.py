71b93e48e5c30bea9c6a93f7ccbfae6f36b438ec
Wen Chuan Lee <leewenchuan.lwc@gmail.com>
2015-09-20 14:21:01 -0500

    Building a tree now works! There was a bug in the provided code I believe, not sure if it was intentional

--- Assignment1/sudokusolver.py
+++ Assignment1/sudokusolver.py
@@ -1,5 +1,4 @@
 import queue
-
 """This is where the problem is defined. Initial state, goal state and other information that can be got from the problem"""
 
 
@@ -7,72 +6,53 @@ class Problem(object):
 
     """
         My notes:
-        - object is the sudoku object, probably a list of lists, 
         - each 0 in the list is an empty node that needs to be solved.
         - need a build initial tree function of nodes that is called by init.
         - since each level is based on numbers, create nodes with value 1 -> length
         - goal test needs to check if the constraints are obeyed.
         - perhaps add a separate flag to enable pruning? (later)
-
-        Problem inherits from Object, so any representation can be used with this class
     """
 
-    def __init__(self, initial, goal=None):
+    def __init__(self, representation, goal=None):
         """This is the constructor for the Problem class. It specifies the initial state, and possibly a goal state, if there is a unique goal.  You can add other arguments if the need arises"""
 
         """
             Based on the set up initial is of type node. A Tree of Nodes.
             Goal is also a node tree.
         """
-        self.initial = initial      # the sudoku representation
+        self.representation = representation
+        self.initial = 0
         self.goal = goal
         self.numberOfUnknowns = 0
-        self.dimension = len(initial) # dimension of sudoku
+        self.dimension = len(representation)  # dimension of sudoku
 
-        for row in initial:
+        for row in self.representation:
             # 0 represents an unknown number
-                self.numberOfUnknowns += row.count(0) 
+            self.numberOfUnknowns += row.count(0)
 
     def actions(self, state):
         """Return the actions that can be executed in the given
         state. The result would typically be a list, but if there are
         many actions, consider yielding them one at a time in an
         iterator, rather than building them all at once."""
-        if state.depth != self.numberOfUnknowns:
-            return "GenerateChildNode"
-        else:
-            return "GenerateSolution"
+        # if state == 0:  #First Node in tree with no child
+        return [x for x in range(1, self.dimension + 1)]
 
     def result(self, state, action):
         """Return the state that results from executing the given
         action in the given state. The action must be one of
         self.actions(state)."""
-        # WC - result returns a node with the action it was created with
-        if action is "GenerateChildNode":
-            for i in range (0, dimension):
-                newNode = Node(i, parent=state, action=action)
-                yield newNode
+        return action     #since we want a bunch of nodes
 
-        if action is "GenerateSolution":
-            while state.parent != self.initial:
-                possibleSolution = state
-                state = state.parent
-                yield possibleSolution
-
-
-
-    def goal_test(self, state):
+    def goal_test(self, collapsedState, depth):
         """Return True if the state is a goal. The default method compares the
         state to self.goal, as specified in the constructor. Override this
         method if checking against a single self.goal is not enough.
         This must be written by students"""
-        if not isinstance(state, int): 
+        if depth != self.numberOfUnknowns:
             return False
-        if state.depth != self.numberOfUnknowns:
-            return False
-        else:
-            # At the bottom of the tree, time to check if solution is complaint.
-            raise(NotImplementedError)
+            # print("not there yet")
+        return False
 
 
 class Node:
@@ -102,20 +82,36 @@ class Node:
 
     def expand(self, problem):
         # List the nodes reachable in one step from this node.
-        # WC: For each action in problem.action return the child_node that is the result of the action
         return [self.child_node(problem, action)
                 for action in problem.actions(self.state)]
 
+    # Makes a child node
     def child_node(self, problem, action):
         next = problem.result(self.state, action)
-        yield Node(next, self, action)
+        return Node(next, self, action)
+
+    def collapse(self):
+        if self.state is 0:
+            return
+        else:
+            x = self.state
+            self = self.parent
+            return [x].append(self.collapse())
+
+    def __str__(self):
+        return "Node %i - Depth %i" % (self.state, self.depth)
+
+# def printTree(parentNode):
+#     print(parentNode)
+#     print("|", end="")
+#     for parentNode.child
 
 
 def breadth_first_search(problem):
     # Start from first node of the problem Tree
     node = Node(problem.initial)
     # Check if current node meets Goal_Test criteria
-    if problem.goal_test(node.state):
+    if problem.goal_test(node.state, node.depth):
         return node
     # Create a Queue to store all nodes of a particular level. Import
     # QueueClass()
@@ -130,25 +126,28 @@ def breadth_first_search(problem):
         # Loop over all children of the current node
         # Note: We consider the fact that a node can have multiple child nodes
         # here
+        print(node)
         for child in node.expand(problem):
+            print(" |--", child, "--", end="")
             # If child node meets Goal_Test criteria
-            if problem.goal_test(child.state):
+            if problem.goal_test(child.collapse(), child.depth):
                 return child
             # Add every new child to the frontier
-            frontier.put(node)
+            frontier.put(child)
+            print("")
+    # printTree(node)
     return None
-
 
 
 def runApp():
     sudoku = [
-              [1, 5, 0, 0, 4, 0],
-              [2, 0, 0, 0, 5, 6],
-              [4, 0, 0, 0, 0, 3],
-              [0, 0, 0, 0, 0, 4],
-              [6, 3, 0, 0, 2, 0],
-              [0, 2, 0, 0, 3, 1],
-             ]
+        [1, 5, 0, 0, 4, 0],
+        [2, 0, 0, 0, 5, 6],
+        [4, 0, 0, 0, 0, 3],
+        [0, 0, 0, 0, 0, 4],
+        [6, 3, 0, 0, 2, 0],
+        [0, 2, 0, 0, 3, 1],
+    ]
     # problemStartNode = Node(sudoku) -- This is done by BFS
     breadth_first_search(Problem(sudoku))
 
