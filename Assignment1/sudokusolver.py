56378ec23a46427e7ba623fdc1972dfd6583af5b
Wen Chuan Lee <leewenchuan.lwc@gmail.com>
2015-09-27 17:20:21 -0500

    Added Hard Sudoku for fun/testing

--- Assignment1/sudokusolver.py
+++ Assignment1/sudokusolver.py
@@ -432,6 +432,18 @@ def runApp():
         [4, 5, 2, 3, 8, 6, 1, 9, 7],
     ]
 
+    nineByNineHard = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 2, 9, 3, 0],
+        [0, 0, 5, 0, 3, 0, 0, 7, 8],
+        [0, 0, 0, 6, 7, 0, 0, 0, 0],
+        [0, 9, 0, 8, 0, 1, 4, 5, 0],
+        [0, 0, 0, 0, 0, 0, 6, 0, 0],
+        [7, 0, 6, 9, 0, 0, 0, 0, 0],
+        [2, 0, 0, 0, 0, 3, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 8, 0],
+    ]
+
     sudoku_driver(fourByFour, solnFourByFour)
     sudoku_driver(sixBySixFirst, solnSixBySixFirst)
     sudoku_driver(sixBySixSecond, solnSixBySixSecond)
