5b7deb41bc4fda922d9f32af823324abc39ce573
Wen Chuan Lee <leewenchuan.lwc@gmail.com>
2015-09-29 01:06:55 -0500

    Argument Parsing, and Forking Partially Implemented

--- minsh.c
+++ minsh.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define DELIMITERS " \n" //space, newlines are delimiters
 
 void shell_loop();
 int getInputWithoutDelimiter(char** input);
 int makeargv(const char *s, const char *delimiters, char ***argvp);
-
+int forkAndExecute(char** argvp, int waitForProcess); 
 
 int main(int argc, char *argv[])
 {
@@ -33,21 +36,55 @@ void shell_loop()
   int run = 1;
   while(run)
   {
-    char *input = NULL;
-
     printf("-$ ");
-    getInputWithoutDelimiter(&input);
-    printf("%s \n", input);
 
-    const char *ex = "exit";
-    
-    if(strcmp(input,ex) == 0)
+    char** argvp;
+    char *input = NULL;
+    size_t buf_len = 0;
+    ssize_t numchars = getline(&input, &buf_len, stdin);
+
+    if(numchars == -1)
+    {
+      fprintf(stderr, "Failed to read input. \n");
+    }
+
+    int argvSize = makeargv(input, DELIMITERS, &argvp);
+
+    // Prevent Segfault if user only hits enter
+    if(argvSize < 1)
+    {
+      free(input);
+      free(argvp);
+      continue; // restart
+    }
+
+    int procStatus;
+    if(strcmp(argvp[0],"exit") == 0 || strcmp(argvp[0],"quit") == 0)
     {
       run = 0;
+      continue;
     }
+
+    else if(strcmp(argvp[argvSize-1],"&") == 0)
+    {
+      procStatus = forkAndExecute(argvp, 0);
+    }
+
+    else
+    {
+      procStatus = forkAndExecute(argvp, 1);
+    }
+
+    /*
+    int i;
+    for (i = 0; i < argvSize; i++)
+    printf("%s \n", argvp[i]);
+    */
     free(input);
-  }
+    free(argvp);
+    }
 }
+
 
 int getInputWithoutDelimiter(char** input)
 {
@@ -60,7 +97,7 @@ int getInputWithoutDelimiter(char** input)
     return -1;
   }
 
-  if(*( *input + numchars -1)  == '\n')
+  if(*(*input + numchars -1)  == '\n')
   {
     *(*input + numchars -1) = '\0'; //null terminator
     --numchars;
@@ -84,7 +121,7 @@ int makeargv(const char *s, const char *delimiters, char ***argvp)
     return -1;
   }
 
-  *argvp = NULL; //pointer to arrray of strings initialiezed to NULL
+  *argvp = NULL; //pointer to array of strings initialized to NULL
 
   /* Pointer arithmethic to make snew point to the real start of the 
    * string, as strspn returns the length of s which consists of 
@@ -124,4 +161,39 @@ int makeargv(const char *s, const char *delimiters, char ***argvp)
   *( (*argvp) + numTokens) = NULL; //put null pointer at end of argvp array
 
   return numTokens;
+}
+
+int forkAndExecute(char** argvp, int waitForProcess)
+{
+  pid_t pid;
+
+  int i=0;
+
+  printf("Time to fork.. \n");
+  pid = fork();
+  int status;
+  if (pid == 0) //child process
+  {
+    execvp(argvp[0], argvp);
+  }
+  else if (pid < 0) //error
+  {
+    fprintf(stderr, "Error Forking Process. \n");
+    status = -1;
+  }
+  else //parent process
+  {
+    if(waitForProcess)
+    {
+      while(waitpid(pid, &status, 0) < 0)
+      {
+	if(errno != EINTR)
+	{
+	  status = -1;
+	  break;
+	}
+      }
+    }
+  }
+  return status;
 }
